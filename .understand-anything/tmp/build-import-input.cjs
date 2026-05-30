687cd05e01f49819325dbbb8361c1fd64fcfbb08
saa938 <ashays@outlook.com>
2026-05-30 12:53:40 -0700

    Remove understand-anything directory from branch

--- .understand-anything/tmp/build-import-input.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const scanData = JSON.parse(fs.readFileSync(path.join('C:', 'Users', 'ashay', 'website', '.understand-anything', 'tmp', 'ua-scan-files.json'), 'utf8'));
-const input = {
-  projectRoot: 'C:\\Users\\ashay\\website',
-  files: scanData.files.map(f => ({ path: f.path, language: f.language, fileCategory: f.fileCategory }))
-};
-fs.writeFileSync(path.join('C:', 'Users', 'ashay', 'website', '.understand-anything', 'tmp', 'ua-import-map-input.json'), JSON.stringify(input, null, 2));
-console.log('Wrote input for', input.files.length, 'files');
